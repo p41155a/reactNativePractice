28220eb41c52aa41330b3c752789ff65189ee9c8
p41155a <50395024+p41155a@users.noreply.github.com>
2020-12-13 04:11:55 +0900

    ✨navigation, stack 기능

--- App.js
+++ App.js
@@ -6,109 +6,85 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import 'react-native-gesture-handler';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    Button,
+    View,
+    Text,
+    TextInput
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const HomeScreen = ({navigation}) => {
+    let [value, setValue] = useState('');
+    return (
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text>Home</Text>
+            <TextInput
+                style={{width:200, height: 40, borderColor: 'gray', borderWidth: 1}}
+                onChangeText={text => {setValue(text)}}
+            />
+            <Button
+                onPress={() => { navigation.navigate('DetailScreen', {value: value})}}
+                title="디테일로 이동"/>
+        </View>
+    )
+}
 
-const App: () => React$Node = () => {
+const DetailScreen = ({route, navigation}) => {
+    useEffect(() => {
+        console.log('Detail Screen appear!');
+        return () => {
+            console.log('Detail Screen disappear!')
+        }
+    }, [navigation])
+    return (
+        <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+            <Text style={{fontSize:20}}>Detail Screen</Text>
+            <Text style={{fontSize: 16, marginVertical: 8}}>Value from Home : {route.params.value}</Text>
+            <Button
+                onPress={() => { navigation.goBack() }}
+                title="뒤로 이동" />
+        </View>
+    )
+}
+
+const SettingsScreen = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>Setting Screen</Text>
+    </View>
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const HomeStack = createStackNavigator();
+const HomeNavigation = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name='HomeScreen' component={HomeScreen} />
+    <HomeStack.Screen name='DetailScreen' component={DetailScreen} />
+  </HomeStack.Navigator>
+)
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+    return (
+       <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen 
+            name = "Home"
+            options={{
+              tabBarIcon: () => (<Ionicons 
+                name="film-outline" size={28}/>) 
+            }}
+            component={HomeNavigation} />
+            <Tab.Screen name = "Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+       </NavigationContainer>
+    );
+};
 
 export default App;
